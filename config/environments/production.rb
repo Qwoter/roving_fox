93fbf709df7f9ab0a010b0f4b0d12bf09d4b8689
Nikita Kovalyov <laqwoter@gmail.com>
2016-02-10 06:29:04 +0200

    misstype

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     # :password       => ENV['pasername'],
     :password       => "nikita",
     :authentication => :plain,
-  }}
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
